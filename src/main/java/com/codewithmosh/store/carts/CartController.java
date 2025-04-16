b4b25051e20dbc485693ab7eb5a54b5c7dff533d
Mosh Hamedani <programmingwithmosh@gmail.com>
2025-04-16 11:01:30 -0400

    Remove Swagger annotations

--- src/main/java/com/codewithmosh/store/carts/CartController.java
+++ src/main/java/com/codewithmosh/store/carts/CartController.java
@@ -17,7 +17,6 @@ import java.util.UUID;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/carts")
-@Tag(name = "Carts")
 public class CartController {
     private final CartService cartService;
 
@@ -32,9 +31,7 @@ public class CartController {
     }
 
     @PostMapping("/{cartId}/items")
-    @Operation(summary = "Adds a product to the cart.")
     public ResponseEntity<CartItemDto> addToCart(
-        @Parameter(description = "The ID of the cart.")
         @PathVariable UUID cartId,
         @RequestBody AddItemToCartRequest request) {
         var cartItemDto = cartService.addToCart(cartId, request.getProductId());
